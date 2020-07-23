c54fd2d95e83bf6f17e115e81782cba2f300b749
Neelanjan Manna <neelanjanmanna@gmail.com>
2020-07-23 21:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ def base():
 
 if __name__ == "__main__":
     app.secret_key = 'qwertyuiop1234567890'
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 3000))
     app.run(debug=True, port=port)
     
